a2f257ba451f2b40753053a0694ed156f7ca354a
Thejas <h.r.thejas@gmail.com>
2023-06-29 00:29:43 +0530

    Create __init__.py

---
+++ src/llmtest/__init__.py
@@ -0,0 +1,53 @@
+import torch
+import os
+from transformers import BitsAndBytesConfig
+from enum import Enum
+from transformers import (
+    AutoModelForCausalLM, 
+    AutoTokenizer, 
+    pipeline,
+    AutoModelForQuestionAnswering,
+    GenerationConfig
+    )
+
+from langchain import (
+    HuggingFaceHub,
+    HuggingFacePipeline
+    )
+
+from langchain.chains import RetrievalQA
+from langchain.embeddings import (
+    HuggingFaceEmbeddings,
+    HuggingFaceInstructEmbeddings
+    )
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.vectorstores import (
+    Chroma,
+    FAISS,
+    ElasticVectorSearch
+    )
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+    Language
+    )
+from langchain.document_loaders import (
+    DirectoryLoader,
+    CSVLoader,
+    EverNoteLoader,
+    PyMuPDFLoader,
+    TextLoader,
+    UnstructuredEmailLoader,
+    UnstructuredEPubLoader,
+    UnstructuredHTMLLoader,
+    UnstructuredMarkdownLoader,
+    UnstructuredODTLoader,
+    UnstructuredPowerPointLoader,
+    UnstructuredWordDocumentLoader,
+    UnstructuredAPIFileLoader,
+)
+
+class IndexType(Enum):
+    FAISS_INDEX = 1
+    CHROMA_INDEX = 2
+    ELASTIC_SEARCH_INDEX = 3
