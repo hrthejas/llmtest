9146b8a8818065682128542056e70680d108d02b
Thejas <h.r.thejas@gmail.com>
2023-06-29 00:08:33 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='llmtest',
+    version='0.0.1',
+    description='Package to load any llm from hf and embedd it with your local html and md data',
+    url='git@github.com:hrthejas/llmtest.git',
+    author='Thejas H.R',
+    author_email='h.r.thejas@gmail.com',
+    license='Apache 2.0',
+    packages=['llmtest'],
+    zip_safe=False
+)
