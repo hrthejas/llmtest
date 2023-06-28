32526bba294a1546b37c5ad352e95c178ac67650
Thejas <h.r.thejas@gmail.com>
2023-06-29 00:14:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='llmtest',
     version='0.0.1',
     description='Package to load any llm from hf and embedd it with your local html and md data',
-    url='git@github.com:hrthejas/llmtest.git',
+    url='git@github.com:hrthejas/llmtest-sdk-package.git',
     author='Thejas H.R',
     author_email='h.r.thejas@gmail.com',
     license='Apache 2.0',
