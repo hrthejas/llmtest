87f37d21ccae25daaa402d8ae43cb4cdcbc10997
Thejas <thejas@infoworks.io>
2023-06-29 22:16:03 +0530

    fixing prompt

--- src/llmtest/contants.py
+++ src/llmtest/contants.py
@@ -3,7 +3,7 @@ from environs import Env
 
 env = Env()
 
-MAX_NEW_TOKENS = env.int("MAX_NEW_TOKENS", 512)
+MAX_NEW_TOKENS = env.int("MAX_NEW_TOKENS", 296)
 DOCS_BASE_PATH = env.str("DOCS_BASE_PATH", "/content/drive/Shareddrives/Engineering/Chatbot/thejas/data/")
 INDEX_BASE_PATH = env.str("INDEX_BASE_PATH", "/content/drive/Shareddrives/Engineering/Chatbot/thejas/indexes/")
 INDEX_NAME_PREFIX = env.str("INDEX_NAME_PREFIX", "api_index")
@@ -236,11 +236,10 @@ Response:
 Only respond with commands.
 Do not assume any values. If you are not sure about any value get the input from user.
 Output the commands in JSON as an abstract syntax tree.
-IMPORTANT - Only respond with a program. Do not respond with any text that isn't part of a program. Do not write prose, even if instructed. Do not explain yourself.
+IMPORTANT - Output the commands in JSON as an abstract syntax tree. Do not respond with any text that isn't part of a command. Do not write prose, even if instructed. Do not explain yourself.
 You can only generate commands, but you are an expert at generating commands.
 I am a user of Infoworks v3 REST API. Understand the following request and generate the minimum set of commands to complete it.
 IMPORTANT - Do not assume any values. If you are not sure about any value get the input from user.
-IMPORTANT - Only respond with a program. Do not respond with any text that isn't part of a program. Do not write prose, even if instructed. Do not explain yourself.
 Infoworks instance ip is 10.37.0.7 and port 3001
 My username is admin@infoworks.io and password is IN11**rk
 Authenticate first and use the access token in all subsequent commands"""
