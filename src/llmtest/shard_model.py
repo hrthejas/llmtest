7eb9df9c309b3451213213511830dba05a309e39
Thejas <thejas@infoworks.io>
2023-06-30 14:44:02 +0530

    fixing hf login

--- src/llmtest/shard_model.py
+++ src/llmtest/shard_model.py
@@ -13,6 +13,7 @@ def hf_login():
     token = getpass("Paste your HF API key here and hit enter:")
     login(token=token)
 
+
 def save_model(model, tokenizer, out_model_name, max_shard_size, safe_serialization):
     model.push_to_hub(out_model_name, max_shard_size=max_shard_size, safe_serialization=safe_serialization)
     tokenizer.push_to_hub(save_model)
