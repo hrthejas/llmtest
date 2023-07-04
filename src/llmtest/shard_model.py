dce6fa552a4838956ff64f098c71c49da24a1241
Thejas <thejas@infoworks.io>
2023-07-04 12:51:30 +0530

    fixing shar model

--- src/llmtest/shard_model.py
+++ src/llmtest/shard_model.py
@@ -16,7 +16,7 @@ def hf_login():
 
 def save_model(model, tokenizer, out_model_name, max_shard_size, safe_serialization):
     model.push_to_hub(out_model_name, max_shard_size=max_shard_size, safe_serialization=safe_serialization)
-    tokenizer.push_to_hub(save_model)
+    tokenizer.push_to_hub(out_model_name)
 
 
 def shard_model(
