5d3e47dafa8e495cb0c57c83de68bd3cdfce1681
Thejas <thejas@infoworks.io>
2023-07-07 11:45:48 +0530

    adding cli query method for debug

--- src/llmtest/iwxchat.py
+++ src/llmtest/iwxchat.py
@@ -166,9 +166,9 @@ def start_iwx_only_chat(local_model_id=constants.DEFAULT_MODEL_NAME,
                                                            index_name_prefix=docs_index_name_prefix,
                                                            docs_base_path=docs_base_path, embeddings=hf_embeddings)
 
-    local_api_vector_store = list()
+    local_api_vector_stores = list()
     for prefix in api_index_name_prefix:
-        local_api_vector_store.append(vectorstore.get_vector_store(index_base_path=index_base_path,
+        local_api_vector_stores.append(vectorstore.get_vector_store(index_base_path=index_base_path,
                                                                    index_name_prefix=prefix,
                                                                    docs_base_path=docs_base_path,
                                                                    embeddings=hf_embeddings))
@@ -183,7 +183,7 @@ def start_iwx_only_chat(local_model_id=constants.DEFAULT_MODEL_NAME,
             search_results = None
             local_qa_chain = None
             if choice_selected == "API":
-                for api_vector_store in local_api_vector_store:
+                for api_vector_store in local_api_vector_stores:
                     if search_results is None:
                         search_results = api_vector_store.similarity_search(query)
                     else:
@@ -228,77 +228,6 @@ def start_iwx_only_chat(local_model_id=constants.DEFAULT_MODEL_NAME,
     interface.launch(debug=debug, share=share_chat_ui)
 
 
-def start_iwx(local_model_id=constants.DEFAULT_MODEL_NAME,
-              docs_base_path=constants.DOCS_BASE_PATH, index_base_path=constants.INDEX_BASE_PATH,
-              docs_index_name_prefix=constants.DOC_INDEX_NAME_PREFIX,
-              api_index_name_prefix=constants.API_INDEX_NAME_PREFIX,
-              max_new_tokens=constants.MAX_NEW_TOKENS, use_4bit_quantization=constants.USE_4_BIT_QUANTIZATION,
-              use_prompt=False, set_device_map=constants.SET_DEVICE_MAP,
-              mount_gdrive=True,
-              share_chat_ui=True, debug=False, gdrive_mount_base_bath=constants.GDRIVE_MOUNT_BASE_PATH,
-              device_map=constants.DEFAULT_DEVICE_MAP, search_type="similarity", search_kwargs={"k": 4},
-              embedding_class=HuggingFaceInstructEmbeddings, model_name="hkunlp/instructor-large", use_queue=True,
-              use_simple_llm_loader=False, is_gptq_model=False, custom_quantization_config=None,
-              use_triton=False, use_safetensors=False, set_torch_dtype=False, torch_dtype=torch.bfloat16
-              ):
-    if mount_gdrive:
-        ingest.mountGoogleDrive(mount_location=gdrive_mount_base_bath)
-
-    local_docs_qa_chain = None
-    local_api_qa_chain = None
-
-    llm = llmloader.load_llm(local_model_id, use_4bit_quantization=use_4bit_quantization, set_device_map=set_device_map,
-                             max_new_tokens=max_new_tokens, device_map=device_map,
-                             use_simple_llm_loader=use_simple_llm_loader, is_quantized_gptq_model=is_gptq_model,
-                             custom_quantiztion_config=custom_quantization_config, use_triton=use_triton,
-                             use_safetensors=use_safetensors, set_torch_dtype=set_torch_dtype, torch_dtype=torch_dtype)
-
-    local_docs_qa_chain, local_api_qa_chain = get_local_qa_chain(llm, embedding_class, model_name,
-                                                                 api_index_name_prefix, docs_base_path,
-                                                                 docs_index_name_prefix, index_base_path,
-                                                                 search_kwargs, search_type, is_openai_model=False)
-
-    choices = ['Docs', 'API']
-    data = [('Bad', '1'), ('Ok', '2'), ('Good', '3'), ('Very Good', '4'), ('Perfect', '5')]
-
-    def chatbot(choice_selected, message):
-        query = get_question(message, use_prompt, constants.API_QUESTION_PROMPT,
-                             choice_selected)
-        reference_docs = "Not populated"
-        if local_api_qa_chain is not None and local_docs_qa_chain is not None:
-            if choice_selected == "API":
-                bot_message = startchat.get_local_model_result(local_api_qa_chain, query)['result']
-            else:
-                bot_message = startchat.get_local_model_result(local_docs_qa_chain, query)['result']
-        else:
-            bot_message = "Seams like iwxchat model is not loaded or not requested to give answer"
-        print(bot_message)
-        print(reference_docs)
-        return bot_message, reference_docs
-
-    msg = gr.Textbox(label="User Question")
-    submit = gr.Button("Submit")
-    choice = gr.inputs.Dropdown(choices=choices, default="Docs", label="Choose question Type")
-    output_textbox = gr.outputs.Textbox(label="IWX Bot")
-    output_textbox.show_copy_button = True
-    output_textbox.lines = 10
-    output_textbox.max_lines = 10
-
-    output_textbox1 = gr.outputs.Textbox(label="Reference Docs")
-    output_textbox1.lines = 2
-    output_textbox1.max_lines = 2
-
-    interface = gr.Interface(fn=chatbot, inputs=[choice, msg], outputs=[output_textbox, output_textbox1],
-                             theme="gradio/monochrome",
-                             title="IWX CHATBOT", allow_flagging="manual", flagging_callback=MysqlLogger(),
-                             flagging_options=data)
-
-    if use_queue:
-        interface.queue().launch(debug=debug, share=share_chat_ui)
-    else:
-        interface.launch(debug=debug, share=share_chat_ui)
-
-
 def query_llm(llm, api_prompt, doc_prompt, api_vector_stores, doc_vector_store, answer_type, query):
     from langchain.chains.question_answering import load_qa_chain
     reference_docs = ""
@@ -325,5 +254,4 @@ def query_llm(llm, api_prompt, doc_prompt, api_vector_stores, doc_vector_store, 
             bot_message = "No matching docs found on the vector store"
     else:
         bot_message = "Seams like iwxchat model is not loaded or not requested to give answer"
-    # record_answers(query, "OPen AI Not configured", bot_message)
     return bot_message, reference_docs
