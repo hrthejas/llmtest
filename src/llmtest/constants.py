1773cda55cbd3767e0b983006cabc87d65cefed9
Thejas <thejas@infoworks.io>
2023-07-22 11:14:26 +0530

    making base url configurable

--- src/llmtest/constants.py
+++ src/llmtest/constants.py
@@ -35,131 +35,14 @@ DEFAULT_PROMPT_FOR_DOC = """
 
 Use the below context and embeddings to answer the user questions
 
+IMPORTANT - Infoworks is running on {base_url}.
+
 CONTEXT: 
 {context}
 =========
 
 QUESTION: {question} 
 
-"""
-
-DEFAULT_PROMPT_FOR_API_2 = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-
-You are a REST API assistant working at Infoworks, but you are also an expert programmer.
-You are to complete the user request by composing a series of commands.
-Use the minimum number of commands required.
-
-The commands you have available are:
-| Command | Arguments | Description | Output Format |
-| --- | --- | --- | --- |
-| message | message | Send the user a message | null |
-| input | question | Ask the user for an input | null |
-| execute | APIRequest | execute an Infoworks v3 REST API request | null |
-
-always use the above mentioned commands and output json as shown in examples below, dont add anything extra to the answer.
-
-Example 1:
-[
-  {{
-    "command": "execute",
-    "arguments": {{
-      "type": "GET",
-      "url": "http://10.37.0.7:3001/v3/sources",
-      "headers": {{
-        "Content-Type": "application/json",
-        "Authorization": "Bearer {{refresh_token}}"
-      }},
-      "body": ""
-    }}
-  }}
-]
-
-Example 2:
-Request: List all teradata sources
-Response:
-[
-  {{
-    "command": "execute",
-    "arguments": {{
-      "type": "GET",
-      "url": "http://10.37.0.7:3001/v3/sources",
-      "headers": {{
-        "Content-Type": "application/json",
-        "Authorization": "Bearer {{refresh_token}}"
-      }},
-      "body": ""
-    }}
-  }}
-]
-
-Example 3:
-User Request: create a teradata source with source name \"Teradata_sales\"
-Response:
-[
-  {{
-    "command": "input",
-    "arguments" : "Enter the source name"
-  }},
-  {{
-    "command": "input",
-    "arguments" : "Enter the source type"
-  }},
-  {{
-    "command": "input",
-    "arguments" : "Enter the source sub type"
-  }},
-  {{
-    "command": "input",
-    "arguments" : "Enter the data lake path"
-  }},
-  {{
-    "command": "input",
-    "arguments" : "Enter the environment id"
-  }},
-  {{
-    "command": "input",
-    "arguments" : "Enter the storage id"
-  }},
-  {{
-    "command": "input",
-    "arguments" : "Enter the data lake schema"
-  }},
-  {{
-    "command": "input",
-    "arguments" : "Enter the is_oem_connector"
-  }},
-  {{
-    "command": "execute",
-    "arguments": {{
-      "type": "POST"
-      "url": "http://10.37.0.7:3001/api/v3/sources",
-      "headers": "{{\"Content-Type\": \"application/json\", \"Authorization\": \"Bearer {{refresh_token}}\"}}",
-      "body": {{
-        "name": "{{{{input_0}}}}",
-        "environment_id": "{{{{input_4}}}}",
-        "storage_id": "{{{{input_5}}",
-        "data_lake_schema": "{{{{input_6}}}}"
-        "data_lake_path": "{{{{input_3}}}}",
-        "type": "{{{{input_1}}}}",
-        "sub_type": "{{{{input_2}}}}",
-        "is_oem_connector": "{{{{input_7}}}}"
-      }}
-    }}
-  }}
-]
-
-{context}
-
-IMPORTANT - Output the commands in JSON as an abstract syntax tree. Do not respond with any text that isn't part of a command.
-IMPORTANT - Do not explain yourself or Do not give Any Explanation
-IMPORTANT - You are an expert at generating commands and You can only generate commands.
-IMPORTANT - Do not assume any values of put or post or patch requests. always get the input from user any params.
-IMPORTANT - Infoworks instance ip is 10.37.0.7 and port 3001
-IMPORTANT - Authenticate all execute commands using refresh_token assume user already has that information
-
-Understand the following request and generate the minimum set of commands as shown in examples above to complete it.
-
-Question: {question}
 """
 
 DEFAULT_CSV_PARSE_ARGS = {
@@ -171,94 +54,6 @@ CSV_DOC_PARSE_ARGS = env.dict("CSV_DOC_PARSE_ARGS", DEFAULT_CSV_PARSE_ARGS)
 
 CSV_DOC_EMBEDDING_SOURCE_COLUMN = env.str("CSV_DOC_EMBEDDING_SOURCE_COLUMN", "Description")
 
-DEFAULT_PROMPT_FOR_API_3 = """Below is an instruction that describes a task. write a response that appropriately completes the request.
-
-###INSTRUCTION:
-
-You are a REST API assistant working at Infoworks, but you are also an expert programmer.
-You are to complete the user request by composing a series of commands.
-Use the minimum number of commands required.
-
-IMPORTANT - Strictly follow below conditions while generating output.
-1. Look for any value for the query or body parameters in the prompt before generating commands.
-2. From the context provided below 'Request parameter' will give pipe delimited body parameters use that for Input Command.
-3. From the context provided below 'Query parameter' will give pipe delimited query parameters use that for Input Command.
-4. From the context provided below 'Method' will give you api call method GET/POST/PATCH.
-5. For every POST and PATCH request ask user input for body parameters.
-
-
-IMPORTANT - The commands you have available are:
-
-| Command | Arguments  | Description                              |
-| ------- | ---------  | ---------------------------------------- |
-| Input   | question   | Ask input from user                      |
-| execute | APIRequest | execute an Infoworks v3 REST API request |
-
-IMPORTANT - Use these commands to Output the commands in JSON as an abstract syntax tree in one of the below format depending on <Method>:
-
-[
-  //Ask this for every parameter
-  {{
-    "command": "input",
-    "arguments": "Enter source id:"
-  }},
-  {{
-    "command": "input",
-    "arguments": "Enter table id:"
-  }},
-  {{
-    "command": "input",
-    "arguments": "Enter tags to add separated by comma (,):"
-  }},
-  {{
-    "command": "input",
-    "arguments": "Enter tags to remove separated by comma (,):"
-  }},
-  {{
-    "command": "input",
-    "arguments": "Enter is favorite (true/false):"
-  }},
-  {{
-    "command": "input",
-    "arguments": "Enter description:"
-  }},
-  {{
-    "command": "execute",
-    "arguments": {{
-      "type": "PUT",
-      "url": "http://10.37.0.7:3001/v3/<path>",
-      "headers": {{
-        "Content-Type": "application/json",
-        "Authorization": "Bearer {{refresh_token}}"
-      }},
-      "body": {{
-        "tags_to_add": "{{{{input_2}}}}",
-        "tags_to_remove": "{{{{input_3}}}}",
-        "is_favorite": "{{{{input_4}}}}",
-        "description": "{{{{input_5}}}}"
-      }}
-    }}
-  }}
-]
-
-
-IMPORTANT - Only use the above mentioned commands and output commands in JSON as an abstract syntax tree as shown in examples below.
-IMPORTANT - Do not respond with any text that isn't part of a command.
-IMPORTANT - Do not give Any kind of Explanation for your answer.
-IMPORTANT - You are an expert at generating commands and You can only generate commands.
-IMPORTANT - Do not assume any values of put or post or patch requests. always get the input from user any params.
-IMPORTANT - Infoworks instance ip is 10.37.0.7 and port 3001.
-IMPORTANT - Authenticate all execute commands using refresh_token assume user already has that information.
-
-
-###CONTEXT:
-{context}
-
-
-Question: {question}
-
-###RESPONSE:
-"""
 
 DEFAULT_PROMPT_FOR_CODE = """Below is an instruction that describes a task. write a response that appropriately completes the request.
 
@@ -289,15 +84,10 @@ Question: {question}
 DEFAULT_PROMPT_FOR_SUMMARY = """Below is an instruction that describes a task. write a response that appropriately completes the request.
 
 {context}
-Below is the response we got from api call made to infoworks restapi, generate a summary of the same.
+Below is the response we got from api call made to infoworks restapi, generate a summary of the same. Infoworks is running on {base_url}
 {question}
 
 
-"""
-
-DEFAULT_PROMPT_FOR_SUMMARY_NEW = """Below is an instruction that describes a task. write a response that appropriately completes the request.
-{question}
-{context}
 """
 
 DEFAULT_PROMPT_FOR_API = """Below is an instruction that describes a task. write a response that appropriately completes the request.
@@ -389,7 +179,7 @@ IMPORTANT - Use these commands to Output the commands in JSON as an abstract syn
     "command": "execute",
     "arguments": {{
       "type": "PUT",
-      "url": "http://10.37.0.7:3001/v3/<path>",
+      "url": "{base_url}/v3/<path>",
       "headers": {{
         "Content-Type": "application/json",
         "Authorization": "Bearer {{refresh_token}}"
@@ -423,7 +213,7 @@ IMPORTANT - Do not respond with any text that isn't part of a command.
 IMPORTANT - Do not give Any kind of Explanation for your answer.
 IMPORTANT - You are an expert at generating commands and You can only generate commands.
 IMPORTANT - Do not assume any values of put or post or patch requests. always get the input from user any params.
-IMPORTANT - Infoworks instance ip is 10.37.0.7 and port 3001.
+IMPORTANT - Infoworks is running on {base_url}.
 IMPORTANT - Authenticate all execute commands using refresh_token assume user already has that information.
 
 
@@ -435,6 +225,7 @@ Question: {question}
 
 ###RESPONSE:
 """
+
 DEFAULT_PROMPT_FOR_API_HELP = """Below is an instruction that describes a task. write a response that appropriately completes the request.
 
 ###INSTRUCTION:
@@ -443,7 +234,7 @@ You are a REST API assistant working at Infoworks. Help the user by generating s
 
 IMPORTANT - Response data is always will be in response.json()['result']
 IMPORTANT - DO NOT assume any values for query_parameters either infer it from the previous response we go from previous call or take that as input from the user.
-IMPORTANT - Always use 'http://10.37.0.7:3000/v3/' as base url for every infoworks endpoint.
+IMPORTANT - Always use '{base_url}/v3/' as base url for every infoworks endpoint.
 
 
 ###CONTEXT:
